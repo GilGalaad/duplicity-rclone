50a1aa94567b3dbe4bd6ada8e53c2eb9f7ad6076
Francesco Magno <francesco@magno.cc>
2016-12-10 15:49:14 +0100

    initial commit

---
+++ rclonebackend.py
@@ -0,0 +1,60 @@
+import os
+import os.path
+
+import duplicity.backend
+from duplicity import path
+from duplicity import log
+
+class RcloneBackend(duplicity.backend.Backend):
+
+	def __init__(self, parsed_url):
+		duplicity.backend.Backend.__init__(self, parsed_url)
+		self.rclone_cmd = "rclone"
+		self.parsed_url = parsed_url
+		self.remote_path = self.parsed_url.path
+		
+		try:
+			p = os.popen(self.rclone_cmd + " version")
+			fout = p.read()
+			ret = p.close()
+		except Exception:
+			pass
+		if not fout:
+			log.FatalError("rclone not found: please install rclone", log.ErrorCode.backend_error)
+
+		if parsed_url.path.startswith('//'):
+			self.remote_path = self.remote_path[2:]
+
+	def _get(self, remote_filename, local_path):
+		temp_dir = os.path.dirname(local_path.name)
+		commandline = "%s copy %s/%s %s" % (self.rclone_cmd, self.remote_path, remote_filename, temp_dir)
+		rc, l, e = self.subprocess_popen(commandline)
+		os.rename(os.path.join(temp_dir, remote_filename), local_path.name)
+
+	def _put(self, source_path, remote_filename):
+		temp_dir = os.path.dirname(source_path.name)
+		temp_filename = os.path.basename(source_path.name)
+		os.rename(source_path.name, os.path.join(temp_dir, remote_filename))
+		commandline = "%s copy --include %s %s %s" % (self.rclone_cmd, remote_filename, temp_dir, self.remote_path)
+		rc, l, e = self.subprocess_popen(commandline)
+		os.rename(os.path.join(temp_dir, remote_filename), source_path.name)
+
+	def _list(self):
+		commandline = "%s mkdir %s" % (self.rclone_cmd, self.remote_path)
+		rc, l, e = self.subprocess_popen(commandline)
+		commandline = "%s ls %s" % (self.rclone_cmd, self.remote_path)
+		rc, l, e = self.subprocess_popen(commandline)
+		ret = []
+		if not l:
+			return ret
+		lines = l.split('\n')
+		for x in lines:
+			if x:
+				ret.append(x.split()[-1])
+		return ret
+
+	def _delete(self, remote_filename):
+		commandline = "%s delete --include %s %s" % (self.rclone_cmd, remote_filename, self.remote_path)
+		rc, l, e = self.subprocess_popen(commandline)
+
+duplicity.backend.register_backend("rclone", RcloneBackend)
